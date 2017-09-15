9dc8745a8b9efb4bccece9bc2c56f6fb1fcf311d
isaac <hzr_bee@163.com>
2017-09-15 17:04:41 +0800

    update

--- server/main.go
+++ server/main.go
@@ -6,17 +6,120 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+const (
+	timeout = 30 * time.Second
+)
+
+var (
+	entering = make(chan handler)
+	leaving  = make(chan handler)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[handler]struct{})
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli.out <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = struct{}{}
+			msg := "online clients:\n"
+			for c := range clients {
+				msg += fmt.Sprintf("- %s\n", c.name)
+			}
+			cli.out <- msg
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.out)
+			close(cli.in)
+		}
+	}
+}
+
+type handler struct {
+	conn net.Conn
+	name string
+	in   chan string
+	out  chan string
+}
+
+func (h *handler) handle() {
+	go h.clientReader()
+	go h.clientWriter()
+
+	timer := time.NewTimer(timeout)
+	h.out <- "input your name:"
+	select {
+	case name := <-h.in:
+		h.name = name
+	case <-timer.C:
+		h.conn.Close()
+		return
+	}
+
+	h.out <- "You are " + h.name
+	messages <- h.name + " has arrived"
+	entering <- *h
+
+LOOP:
+	for {
+		select {
+		case msg := <-h.in:
+			messages <- h.name + ": " + msg
+			timer.Reset(timeout)
+		case <-timer.C:
+			break LOOP
+		}
+	}
+
+	leaving <- *h
+	messages <- h.name + " has left"
+
+	h.conn.Close()
+}
+
+func (h *handler) clientWriter() {
+	for msg := range h.out {
+		fmt.Fprintln(h.conn, msg)
+	}
+}
+
+func (h *handler) clientReader() {
+	scanner := bufio.NewScanner(h.conn)
+	for scanner.Scan() {
+		h.in <- scanner.Text()
+	}
+}
+
+func (h *handler) user() {
+
+}
+
+func newHandler(conn net.Conn) *handler {
+	return &handler{
+		conn: conn,
+		in:   make(chan string),
+		out:  make(chan string),
+	}
+}
 
 var (
 	port = flag.String("port", "20000", "port")
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:"+*port)
+	addr := "localhost:" + *port
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("listening at", addr)
 	go broadcaster()
 	for {
 		conn, err := listener.Accept()
@@ -24,57 +127,7 @@ func main() {
 			log.Print(err)
 			continue
 		}
-		go handleConn(conn)
-	}
-}
-
-type client chan<- string
-
-var (
-	entering = make(chan client)
-	leaving  = make(chan client)
-	messages = make(chan string)
-)
-
-func broadcaster() {
-	clients := make(map[client]bool)
-	for {
-		select {
-		case msg := <-messages:
-			for cli := range clients {
-				cli <- msg
-			}
-		case cli := <-entering:
-			clients[cli] = true
-		case cli := <-leaving:
-			delete(clients, cli)
-			close(cli)
-		}
-	}
-}
-
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-
-	ch := make(chan string)
-	go clientWriter(conn, ch)
-
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
-
-	input := bufio.NewScanner(conn)
-	for input.Scan() {
-		messages <- who + ": " + input.Text()
-	}
-
-	leaving <- ch
-	messages <- who + " has left"
-}
-
-func clientWriter(conn net.Conn, ch <-chan string) {
-	for msg := range ch {
-		fmt.Fprintln(conn, msg)
+		handler := newHandler(conn)
+		go handler.handle()
 	}
 }
